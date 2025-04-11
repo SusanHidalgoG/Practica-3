a06fb678cf3cd74a2f1aea89127cf1d44b577294
EstebanQuesada <equesada00666@ufide.ac.cr>
2025-04-10 23:22:56 -0600

    proyecto funcional

---
+++ Practica_3/Practica_3/Controllers/AbonosController.cs
@@ -0,0 +1,46 @@
+﻿using Practica_3.Models;
+using System.Web.Mvc;
+using System;
+
+public class AbonosController : Controller
+{
+    private SistemaRegistroEntities db = new SistemaRegistroEntities();
+
+
+    [HttpGet]
+    public ActionResult Registro()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult RegistrarAbono(int CompraId, decimal MontoAbono)
+    {
+        var compra = db.Compras.Find(CompraId);
+
+        if (compra == null || MontoAbono <= 0 || MontoAbono > compra.Saldo)
+        {
+            TempData["Error"] = "Datos inválidos para el abono.";
+            return RedirectToAction("Registro", "Abonos");
+        }
+
+        Abonos nuevoAbono = new Abonos
+        {
+            CompraId = CompraId,
+            Monto = MontoAbono,
+            Fecha = DateTime.Now
+        };
+
+        db.Abonos.Add(nuevoAbono);
+        compra.Saldo -= MontoAbono;
+
+        if (compra.Saldo <= 0)
+        {
+            compra.Estado = "Cancelado";
+        }
+
+        db.SaveChanges();
+
+        return RedirectToAction("Consulta", "Compras");
+    }
+}
---
+++ Practica_3/Practica_3/Controllers/ComprasController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Practica_3.Models;
+
+namespace Practica_3.Controllers
+{
+    public class ComprasController : Controller
+    {
+        private SistemaRegistroEntities db = new SistemaRegistroEntities();
+
+   
+        public ActionResult Consulta()
+        {
+            var compras = db.Compras.OrderBy(c => c.Estado).ToList();
+            return View(compras);
+        }
+
+        public JsonResult ObtenerComprasPendientes()
+        {
+            var compras = db.Compras
+                            .Where(c => c.Estado == "Pendiente")
+                            .Select(c => new
+                            {
+                                c.CompraId,
+                                c.Descripcion,
+                                c.Saldo
+                            })
+                            .ToList();
+
+            return Json(compras, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public JsonResult ObtenerSaldo(int compraId)
+        {
+            var saldo = db.Compras
+                          .Where(c => c.CompraId == compraId)
+                          .Select(c => c.Saldo)
+                          .FirstOrDefault();
+
+            return Json(saldo, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
